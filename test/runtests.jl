52de3398fc3d0533632b05b81060e6dcb94b3d1b
Josiah Putman <joshikatsu@gmail.com>
2020-11-14 16:22:04 -0800

    Moving test data

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,22 +2,21 @@ using JAMS
 using Test
 
 function test_contact(;
-        filename::String="../testdata/contact.jams",
+        filename::String="data/contact.jams",
         contact=JAMS.Contact("Josiah Putman", "joshikatsu@gmail.com"))
-    # JAMS.save(filename, contact)
     loaded_contact = JAMS.load(filename, JAMS.Contact)
     @test contact == loaded_contact
 end
 
 function load_annotation(;
-        filename::String="../testdata/annotation.jams")
+        filename::String="data/annotation.jams")
     loaded_annotation::JAMS.Annotation = JAMS.load(filename, JAMS.Annotation)
     @test loaded_annotation.namespace == "pitch_contour"
 end
 
 
 function load_annotations(;
-    filename::String="../testdata/annotations.jams")
+    filename::String="data/annotations.jams")
     loaded_annotations::JAMS.Annotations = JAMS.load(filename)
     @test loaded_annotation.file_metadata.title == "00_BN1-129-Eb_comp"
 end
